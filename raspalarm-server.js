65b0e61483f9d704b204ed0d1c8ac9fc9ff1974e
Olivier Chevallier <olivier.chevallier@me.com>
2020-07-08 18:58:31 +0200

    Connection test with db

--- raspalarm-server.js
+++ raspalarm-server.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var mongodb = require('mongodb');
+
 var app = express();
+var MongoClient = mongodb.MongoClient;
+
 const PORT = process.env.PORT || 5005;
+const DB_URL = "mongodb://localhost:27017/raspalarm";
+
+MongoClient.connect(DB_URL, function(err, db) {
+  if (err) throw err;
+  console.log("Connected to database !");
+  db.close();
+});
 
 app.get('/', (req, res, next) => {
   res.send('Hello world !');
